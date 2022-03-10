5cc34b1d1428f3a3acad6fa9a2b9c7b16ae52fe4
FelipeRibeiroLabs <frlabsmd@gmail.com>
2022-03-10 11:10:57 -0300

    "First Commit: Project Initialized "

---
+++ src/main/java/br/com/frlabs/apimarketplace/ApiMarketPlaceApplication.java
@@ -0,0 +1,13 @@
+package br.com.frlabs.apimarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMarketPlaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMarketPlaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/frlabs/apimarketplace/ApiMarketPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.frlabs.apimarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMarketPlaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
