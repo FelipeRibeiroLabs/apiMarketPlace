2f5d37a0aadd0a3324724b5ac6cebaa0c9639331
FelipeRibeiroLabs <frlabsmd@gmail.com>
2022-03-18 23:05:18 -0300

    "Feat: Test exception ResourceExceptionHandler"

---
+++ src/test/java/br/com/frlabs/apimarketplace/resource/exceptions/ResourceExceptionHandlerTest.java
@@ -0,0 +1,34 @@
+package br.com.frlabs.apimarketplace.resource.exceptions;
+
+import br.com.frlabs.apimarketplace.services.exceptions.ObjectNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ResourceExceptionHandlerTest {
+
+    @InjectMocks
+    private ResourceExceptionHandler resourceExceptionHandler;
+
+    @Test
+    void when_objectNotFound_thenReturn_responseEntityStandartError() {
+        ResponseEntity<StandartError> response = resourceExceptionHandler.objectNotFound(
+                new ObjectNotFoundException("Object not found!!"),
+                new MockHttpServletRequest()
+        );
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandartError.class, response.getBody().getClass());
+        assertEquals("Object not found!!", response.getBody().getError());
+        assertEquals(404, response.getBody().getStatus());
+    }
+
+}
